9c936b1855e4bac22e8c9c073ad4471ffd6fd817
Pasquale De Nisi <pasqualedenisi98@live.it>
2024-11-18 18:34:53 +0100

    refactor(code): remove some redundances

--- src/main/java/scrum/attendance_app/controller/AuthenticationController.java
+++ src/main/java/scrum/attendance_app/controller/AuthenticationController.java
@@ -14,6 +14,8 @@ import scrum.attendance_app.Service.RegistrationService;
 import scrum.attendance_app.data.dto.ProfessorDTO;
 import scrum.attendance_app.data.dto.StudentDTO;
 import scrum.attendance_app.data.entities.Student;
+import scrum.attendance_app.error_handling.exceptions.InvalidRoleException;
+import scrum.attendance_app.error_handling.exceptions.TokenCreationException;
 import scrum.attendance_app.security.TokenStore;
 import scrum.attendance_app.security.UserDetailsServiceImpl;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -50,7 +52,7 @@ public class AuthenticationController {
 
     @PostMapping(path = "/registerStudent")
     public ResponseEntity<String> registerStudent(@RequestBody StudentDTO studentDTO) {
-        String registrationStatus = registrationService.registerStudent(studentDTO);
+        registrationService.registerStudent(studentDTO);
         return new ResponseEntity<>("Registered successfully", HttpStatus.OK);
     }
 
@@ -68,65 +70,30 @@ public class AuthenticationController {
 
     @PostMapping(path = "/authenticate")
     @ResponseStatus(HttpStatus.OK)
-    public void authenticate(@RequestParam("username") String email, @RequestParam("password") String password, HttpServletResponse response) throws JOSEException, JOSEException {
+    public void authenticate(@RequestParam("username") String email, @RequestParam("password") String password, HttpServletResponse response) throws JOSEException {
         try {
-            System.out.println("Inizio autenticazione per l'utente: " + email);
-
-            // Autenticazione dell'utente
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
-
-            // Determina il ruolo dell'utente in base all'email usando UserDetailsService
             String role = null;
-
-            // Carica i dettagli dell'utente
-            System.out.println("Carico i dettagli dell'utente da UserDetailsService per: " + email);
             UserDetails userDetails = userDetailsService.loadUserByUsername(email);
-
-            // Verifica il ruolo in base al tipo di utente
-            System.out.println("Dettagli dell'utente: " + userDetails.getUsername());
-            userDetails.getAuthorities().forEach(authority -> System.out.println("Autorità trovata: " + authority.getAuthority()));
-
             if (userDetails.getAuthorities().stream().anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_STUDENT"))) {
                 role = "studente";
-                System.out.println("Ruolo dell'utente: studente");
             } else if (userDetails.getAuthorities().stream().anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_PROFESSOR"))) {
                 role = "professore";
-                System.out.println("Ruolo dell'utente: professore");
             } else {
-                throw new RuntimeException("Ruolo utente non riconosciuto.");
+                throw new InvalidRoleException("Ruolo utente non riconosciuto.");
             }
-
-            // Creazione del token JWT con il ruolo dell'utente
             try {
-                System.out.println("Creazione del token JWT per il ruolo: " + role);
                 String token = TokenStore.getInstance().createToken(Map.of("email", email, "role", role));
-                System.out.println(token);
-
-                // Aggiungi il token JWT nell'header della risposta
                 response.addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
-                System.out.println("Token JWT generato con successo.");
-
             } catch (Exception e) {
-                throw new RuntimeException("Errore nella creazione del token: " + e.getMessage());
+                throw new TokenCreationException("Errore nella creazione del token: " + e.getMessage());
             }
 
         } catch (UsernameNotFoundException e) {
-            System.out.println("Errore: utente non trovato con email: " + email);
             throw new RuntimeException("Utente non trovato: " + email);
-        } catch (RuntimeException e) {
+        } catch (RuntimeException | TokenCreationException e) {
             e.printStackTrace();
-            System.out.println("Errore di autenticazione per l'utente: " + email + " - Dettaglio errore: " + e.getMessage());
             throw new RuntimeException("Credenziali errate per l'utente: " + email + " - Errore: " + e.getMessage());
         }
     }
-
-    @PostMapping(path = "/authenticate2")
-    @ResponseStatus(HttpStatus.OK)
-    public void authenticate2(@RequestParam("username") String email, @RequestParam("password") String password, HttpServletResponse response) throws JOSEException, JOSEException {
-        Optional<Student> studentByEmail = registrationService.getStudentByEmail(email);
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
-        System.out.println("ciao");
-    }
-
-
 }
---
+++ src/main/java/scrum/attendance_app/error_handling/exceptions/TokenCreationException.java
@@ -0,0 +1,6 @@
+package scrum.attendance_app.error_handling.exceptions;
+
+public class TokenCreationException extends Throwable {
+    public TokenCreationException(String s) {
+    }
+}
